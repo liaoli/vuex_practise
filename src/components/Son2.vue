132e2d41d0444684c9de67fce808335ee84f0e50
liaoli <277218962@qq.com>
2023-09-15 11:47:52 +0800

    mapActions

--- src/components/Son2.vue
+++ src/components/Son2.vue
@@ -1,31 +1,34 @@
 <template>
-    <div class="box">
-      <h2>Son2 子组件</h2>
-      从vuex中获取的值:<label></label>
-      <br />
-      <button @click="subtractCount">值 - 1</button>
-    </div>
-  </template>
+  <div class="box">
+    <h2>Son2 子组件</h2>
+    从vuex中获取的值:<label></label>
+    <br />
+    <button @click="subtractCount">值 - 1</button>
+    <button @click="setAsyncCount(300)">1秒钟之后改成300</button>
+  </div>
+</template>
   
   <script>
-  export default {
-    name: 'Son2Com',
-    methods:{
-      subtractCount(){
-        this.$store.commit('subtractCount',5)
-      }
-    }
-  }
-  </script>
+import { mapActions } from "vuex";
+export default {
+  name: "Son2Com",
+  methods: {
+    subtractCount() {
+      this.$store.commit("subtractCount", 5);
+    },
+    ...mapActions(["setAsyncCount"]),
+  },
+};
+</script>
   
   <style lang="css" scoped>
-  .box {
-    border: 3px solid #ccc;
-    width: 400px;
-    padding: 10px;
-    margin: 20px;
-  }
-  h2 {
-    margin-top: 10px;
-  }
-  </style>
+.box {
+  border: 3px solid #ccc;
+  width: 400px;
+  padding: 10px;
+  margin: 20px;
+}
+h2 {
+  margin-top: 10px;
+}
+</style>
